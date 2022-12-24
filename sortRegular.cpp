b83295cbe54112dbf40fdedae21c55e854af8573
Noam <bitton.noam@campus.technion.ac.il>
2022-12-24 12:36:04 +0200

    rank tree

---
+++ sortRegular.cpp
@@ -0,0 +1,11 @@
+#include "SortRegular.h"
+
+bool SortRegular::equalTo(int n1, int n2) {
+    return n1==n2;
+}
+
+bool SortRegular::lessThan(int n1, int n2) {
+    return n1 < n2;
+}
+
+
---
+++ sortRegular.h
@@ -0,0 +1,19 @@
+//
+// Created by User on 24/12/2022.
+//
+
+#ifndef WET2_SORTREGULAR_H
+#define WET2_SORTREGULAR_H
+
+class SortRegular {
+public:
+    SortRegular() = default;
+    ~SortRegular() = default;
+    static bool equalTo(int n1, int n2);
+
+    //static bool greaterThan(int id1, int id2);
+
+    static bool lessThan(int n1, int n2);
+};
+
+#endif //WET2_SORTREGULAR_H
